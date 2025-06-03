aff2df2ba697a1f051e6a104f87c48648a41d564
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-03 10:07:34 -0600

    Messages route changes

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,66 +1,77 @@
 import {Request,Response} from 'express'
 import Messages from '../models/messageSchema';
+import mongoose from 'mongoose';
 
 async function getAllMessages(req:Request, res:Response) {
     const allMessages = await Messages.find({});
-    res.json(allMessages);
+    res.status(200).json(allMessages);
 }
 
-
-async function getAllMessagesByStatusOrLicencePlate(req:Request, res:Response) {
-    let messages = await Messages.find({ truckLicencePlate: req.params.statusOrLicencePlate });
-    if (!messages || messages.length == 0) {
-        messages = await Messages.find({ status: req.params.statusOrLicencePlate });
-        if (!messages || messages.length == 0) {
-            res.json({ err: `Cannot find messages with license plate or status ${req.params.statusOrLicencePlate}` });
-        } else {
-            res.json(messages);
-        }
-    } else {
-        res.json(messages);
+async function getMessagesByTruckId(req:Request, res:Response) {
+    const truckId = req.params.truckId;
+    if (!truckId || !mongoose.Types.ObjectId.isValid(truckId)) {
+        res.status(200).json({}); //returning empty object
+        return;
     }
+    const allMessages = await Messages.find({truck:req.params.truckId});
+    res.status(200).json(allMessages);
 }
 
-async function postNewMessage(req:Request, res:Response) {
+
+// async function getAllMessagesByStatusOrLicencePlate(req:Request, res:Response) {
+//     let messages = await Messages.find({ truckLicencePlate: req.params.statusOrLicencePlate });
+//     if (!messages || messages.length == 0) {
+//         messages = await Messages.find({ status: req.params.statusOrLicencePlate });
+//         if (!messages || messages.length == 0) {
+//             res.json({ err: `Cannot find messages with license plate or status ${req.params.statusOrLicencePlate}` });
+//         } else {
+//             res.json(messages);
+//         }
+//     } else {
+//         res.json(messages);
+//     }
+// }
+
+async function createNewMessage(req:Request, res:Response) {
     delete req.body.status; //date and status should be schemas default
     delete req.body.date;
     const newMessage = await Messages.create(req.body);
-    res.json(newMessage);
+    res.status(201).json(newMessage);
 }
 
-async function updateMessageById(req:Request, res:Response) {
-    req.body.date = Date.now();
-    let updatedMessage = await Messages.findById(req.params.id);
-    if (!updatedMessage) {
-        res.json({ err: `Cannot find message with id ${req.params.id}` });
-    } else if (updatedMessage.status != 'pending') {
-        res.json({ err: `Cannot change message witch in not 'pending' id:${req.params.id}` });
-    } else {
-        await updatedMessage.updateOne(req.body, { new: true });
-        res.json(updatedMessage);
-    }
-}
+// async function updateMessageById(req:Request, res:Response) {
+//     req.body.date = Date.now();
+//     let updatedMessage = await Messages.findById(req.params.id);
+//     if (!updatedMessage) {
+//         res.json({ err: `Cannot find message with id ${req.params.id}` });
+//     } else if (updatedMessage.status != 'pending') {
+//         res.json({ err: `Cannot change message witch in not 'pending' id:${req.params.id}` });
+//     } else {
+//         await updatedMessage.updateOne(req.body, { new: true });
+//         res.json(updatedMessage);
+//     }
+// }
 
 async function deleteMessageById(req:Request, res:Response) {
-    const deletedMessage = await Messages.findById(req.params.id);
+    const deletedMessage = await Messages.findById(req.params.messageId);
     if (!deletedMessage) {
-        res.json({ err: `Cannot find message with id ${req.params.id}` })
+        res.json({ err: `Cannot find message with id ${req.params.messageId}` })
     } else if (deletedMessage.status != 'pending') {
-        res.json({ err: `Cannot delete message witch in not 'pending' id:${req.params.id}` });
+        res.json({ err: `Cannot delete message witch in not 'pending' id:${req.params.messageId}` });
     } else {
         await Messages.findByIdAndDelete(req.params.id);
-        res.json(deletedMessage);
+        res.status(200).json(deletedMessage);
     }
 }
 
-async function searchForText(req:Request, res:Response) {
-    let messages = await Messages.find({ $text: { $search: req.params.text } });
-    if (!messages || messages.length == 0) {
-        res.json({ err: `Cannot find messages with text:${req.params.text}` });
-    } else {
-        res.json(messages);
-    }
-}
+// async function searchForText(req:Request, res:Response) {
+//     let messages = await Messages.find({ $text: { $search: req.params.text } });
+//     if (!messages || messages.length == 0) {
+//         res.json({ err: `Cannot find messages with text:${req.params.text}` });
+//     } else {
+//         res.json(messages);
+//     }
+// }
 
-export default { getAllMessages, getAllMessagesByStatusOrLicencePlate, postNewMessage, updateMessageById, deleteMessageById, searchForText }
+export default { getAllMessages,createNewMessage,deleteMessageById,getMessagesByTruckId }
 
--- src/routes/messageRoute.ts
+++ src/routes/messageRoute.ts
@@ -4,11 +4,12 @@ import Auth from '../middleware/auth';
 
 const router = express.Router();
 
-router.get('/',messageCTRL.getAllMessages);
-router.get('/:statusOrLicencePlate',messageCTRL.getAllMessagesByStatusOrLicencePlate);
-router.post('/',messageCTRL.postNewMessage);
-router.put('/:id',messageCTRL.updateMessageById);
-router.delete('/:id',messageCTRL.deleteMessageById);
-router.get('/search/:text',messageCTRL.searchForText)
+router.get('/',Auth.auth,Auth.dispatcherAuth,messageCTRL.getAllMessages);
+router.get('/:truckId',Auth.auth,messageCTRL.getMessagesByTruckId); //any authorised user can read thouse
+// router.get('/:statusOrLicencePlate',messageCTRL.getAllMessagesByStatusOrLicencePlate);
+router.post('/',Auth.auth,Auth.dispatcherAuth,messageCTRL.createNewMessage);
+// router.put('/:id',messageCTRL.updateMessageById);
+router.delete('/:messageId',Auth.auth,Auth.dispatcherAuth,messageCTRL.deleteMessageById);
+// router.get('/search/:text',messageCTRL.searchForText)
 
 export default router
