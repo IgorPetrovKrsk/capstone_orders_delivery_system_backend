ddacd7abc2c34cf3d4a72821a57eac85ea51479e
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-05 20:26:25 -0600

    aws controller now returns static URL

--- src/controllers/awsCTRL.ts
+++ src/controllers/awsCTRL.ts
@@ -6,25 +6,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 const awsBucketName = process.env.AWS_BUCKET_NAME;
 
+const region = "us-east-2";
 const s3 = new S3Client({
-  region: "us-east-2",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
-  },
+    region: region,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    },
 });
 
 export async function getAwsUrl(req: Request, res: Response) {
     const key = `uploads/${Date.now()}.jpg`
+
     const command = new PutObjectCommand({
         Bucket: awsBucketName,
         Key: key,
         ContentType: "image/jpeg",
     });
 
+    const staticUrl = `https://${awsBucketName}.s3.${region}.amazonaws.com/${key}`;
     const url = await getSignedUrl(s3, command, { expiresIn: 60 });
 
-    res.json({ url,key });
+    res.json({ url, staticUrl });
 }
 
 export default { getAwsUrl }
