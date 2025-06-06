aec3c0e85ff43d7a9de3e9794d7ae3e960009770
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-06 14:25:02 -0600

    delete order by id added

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,32 +1,38 @@
-import {Request,Response} from 'express'
+import { Request, Response } from 'express'
 import Orders from '../models/orderSchema';
 
-async function getAllOrders(req:Request, res:Response) {
+async function getAllOrders(req: Request, res: Response) {
     const allOrders = await Orders.find({});
     res.json(allOrders);
 }
 
-async function getOrdersByTruckId(req:Request, res:Response) {
+async function getOrdersByTruckId(req: Request, res: Response) {
     const orders = await Orders.find({ truck: req.params.truckId });
     if (!orders || orders.length == 0) {
-        res.status(400).json({ error: [{ msg: `Cannot find orders for truck with ID ${req.params.truckId}`}] });
+        res.status(400).json({ error: [{ msg: `Cannot find orders for truck with ID ${req.params.truckId}` }] });
         return;
     } else {
         res.json(orders);
     }
 }
 
-async function postNewOrder(req:Request, res:Response) {
+async function postNewOrder(req: Request, res: Response) {
     delete req.body.status; //date and status should be schemas default
     delete req.body.date;
-    const newOrder = await Orders.create(req.body);    
+    const newOrder = await Orders.create(req.body);
     res.status(201).json(newOrder);
 }
 
-async function deleteDelivered(req:Request, res:Response) {
+async function deleteDelivered(req: Request, res: Response) {
     await Orders.deleteMany({ status: 'delivered' });
-    res.status(200).json({ status: [{ msg: `All delivered orders has been deleted.` }] });    
+    res.status(200).json({ status: [{ msg: `All delivered orders has been deleted.` }] });
 }
 
-export default { getAllOrders, postNewOrder, deleteDelivered, getOrdersByTruckId }
+async function deleteOrderById(req: Request, res: Response) {
+    const deletedOrder = await Orders.findOneAndDelete({ _id: req.params.orderId });
+    res.status(200).json({ status: [{ msg: `Order ${deletedOrder?._id} has been deleted.` }] });
+    return;
+}
+
+export default { getAllOrders, postNewOrder, deleteDelivered, getOrdersByTruckId, deleteOrderById }
 
--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.get('/',Auth.auth,Auth.dispatcherAuth,orderCTRL.getAllOrders);
 router.post('/',Auth.auth,Auth.dispatcherAuth,orderCTRL.postNewOrder);
-router.delete('/deleteDelivered',Auth.auth,Auth.dispatcherAuth,orderCTRL.deleteDelivered);
+router.delete('/:orderId',Auth.auth,Auth.dispatcherAuth,orderCTRL.deleteOrderById);
+//router.delete('/deleteDelivered',Auth.auth,Auth.dispatcherAuth,orderCTRL.deleteDelivered);
 
 
 export default router
