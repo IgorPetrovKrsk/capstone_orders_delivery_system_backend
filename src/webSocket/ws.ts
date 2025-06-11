66d6e45ecb7705447a0ff20bf465cc339cb21331
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-06-11 14:18:03 -0600

    sending messages from dispatcher to driver done

--- src/webSocket/ws.ts
+++ src/webSocket/ws.ts
@@ -16,21 +16,34 @@ export default function startWebSocket() {
 
     const wss_port = Number(process.env.WEB_SOCKET_PORT) ?? 3001;
     const wss = new WebSocket.Server({ port: wss_port });
+    const userSocketArray: Array<{ user:any ; ws: WebSocket }> = [];
+
 
     wss.on('connection', (ws) => {
         logger.info('Client connected');
-        
-        
-        ws.send('Welcome');
 
         ws.on('message', (message) => {
             logger.info(`'received:', ${message}`);
-            ws.send(`Server received: ${message}`);
+            ws.send(`Server received: ${message}`); //sending back just for test should comment prior relise
+
+            const data = JSON.parse(message.toString());
+            if (data.user) {
+                const user = data.user;
+                userSocketArray.push({user,ws})                
+                return;
+            }
+
+            if (data.truck && data.message){
+                const recieverWs = userSocketArray.find(it=> it.user.truck == data.truck._id)?.ws;
+                if (recieverWs){
+                    recieverWs.send(data.message);
+                }
+            }
         });
 
     });
 
-    console.log(`WebSocket server started on port ${wss_port}`);    
+    console.log(`WebSocket server started on port ${wss_port}`);
 
 }
 
